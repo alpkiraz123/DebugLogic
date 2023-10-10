0ea09bfda0f00efba1a12c4ed6dd3d1fc7f2e10b
alpkiraz123 <79788142+alpkiraz123@users.noreply.github.com>
2023-10-10 21:34:57 +0300

    Version 1 Basic.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.debuglogic {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.debuglogic to javafx.fxml;
+    exports com.example.debuglogic;
+}
